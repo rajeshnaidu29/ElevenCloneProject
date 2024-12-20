6f2eb449cf7402bf42a4ec022b2244b9fbc4a585
rajeshnaidu29 <rajeshgolla1129>
2024-12-20 22:16:13 +0530

    ch

--- ElevenCloneProject/settings.py
+++ ElevenCloneProject/settings.py
@@ -122,14 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
 STATICFILES_DIRS=[BASE_DIR/'Staticfiles']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
